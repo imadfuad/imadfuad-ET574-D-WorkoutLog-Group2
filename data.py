47e9d0761af76d272c37d617033657fed86601e4
Jahidul Islam <jahidul.islam79@student.qcc.cuny.edu>
2026-03-14 00:45:12 +0000

    Student C: Updated issue tracker with summary_report

--- data.py
+++ data.py
@@ -1,5 +1,3 @@
 # data.py
-
-# Temporarily hardcoding data so you can test your functions!
-workout_types = ["Running", "Cycling", "Weightlifting"]
-workout_minutes = [30, 45, 60]
+workout_types = []
+workout_minutes = []
